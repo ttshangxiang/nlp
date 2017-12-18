61906bbde1a5ea78bae5bb664c3c8d4b26358895
jianghuaqing <jianghuaqing@haizhi.com>
2017-12-18 19:04:34 +0800

    开4个线程

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 ./node_modules/.bin/rimraf ./dist/*
 ./node_modules/.bin/webpack
 ./node_modules/.bin/pm2 delete nlp
-./node_modules/.bin/pm2 start server.js --name "nlp"
+./node_modules/.bin/pm2 start server.js --name "nlp" -i 4
